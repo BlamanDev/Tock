2646231e0537bfac32177d7a71efc585c51f34bb
Remy Rouinvy <blaman.net@laposte.net>
2017-08-23 12:07:22 +0200

    Minor improvements
    - madded some minor improvements to the card system

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -7,6 +8,10 @@ public class Card : NetworkBehaviour
 {
     public CardsColorsEnum Color;
     public CardsValuesEnum Value;
+
+    public delegate void CardEffect(Pawn target);
+    public CardEffect Effect;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -22,10 +27,139 @@ public class Card : NetworkBehaviour
         Color = color;
         Value = value;
         this.name = value.ToString() + "_" + color.ToString();
+        Effect = getCardEffect(value);
+        
+        this.gameObject.GetComponentInChildren<MeshRenderer>().material = Resources.Load<Material>("\\Materials\\"+ this.name);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    #region Card Effect
+    private CardEffect getCardEffect(CardsValuesEnum value)
     {
-        //GetComponent<Rigidbody>().useGravity = false;
+        CardEffect methodChosen = null;
+        switch (value)
+        {
+            case CardsValuesEnum.ACE:
+                methodChosen = ACE;
+                break;
+            case CardsValuesEnum.TWO:
+                methodChosen = TWO;
+                break;
+            case CardsValuesEnum.THREE:
+                methodChosen = THREE;
+                break;
+            case CardsValuesEnum.FOUR:
+                methodChosen = FOUR;
+                break;
+            case CardsValuesEnum.FIVE:
+                methodChosen = FIVE;
+                break;
+            case CardsValuesEnum.SIX:
+                methodChosen = SIX;
+                break;
+            case CardsValuesEnum.SEVEN:
+                methodChosen = SEVEN;
+                break;
+            case CardsValuesEnum.EIGHT:
+                methodChosen = EIGHT;
+                break;
+            case CardsValuesEnum.NINE:
+                methodChosen = NINE;
+                break;
+            case CardsValuesEnum.TEN:
+                methodChosen = TEN;
+                break;
+            case CardsValuesEnum.JACK:
+                methodChosen = JACK;
+                break;
+            case CardsValuesEnum.QUEEN:
+                methodChosen = QUEEN;
+                break;
+            case CardsValuesEnum.KING:
+                methodChosen = KING;
+                break;
+            case CardsValuesEnum.JOKER:
+                methodChosen = JOKER;
+                break;
+            default:
+                break;
+        }
+        return methodChosen;
+    }
+
+    private void JOKER(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void KING(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void QUEEN(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void JACK(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TEN(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void NINE(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EIGHT(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SEVEN(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SIX(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FIVE(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FOUR(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void THREE(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void TWO(Pawn target)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ACE(Pawn target)
+    {
+
+    }
+    #endregion
+
+public void Move(Pawn target)
+    {
+        target.Move((int)Value);
+        Effect(target);
     }
 }
